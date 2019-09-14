b7abdd2151ee68d9f2f4fa03d4deedd77fa3a3aa
gabb134 <cg.marrero@cgodin.qc.ca>
2019-09-14 14:48:13 -0400

    Creation des tables projet 1 (samedi 14 sept 2019)

--- CreationTableManuelle.sql
+++ CreationTableManuelle.sql
@@ -120,3 +120,22 @@ create table ReservationChambre (
 	NbPersonnes int,
 	constraint pk_Reservation primary key(NoClient, NoChambre, DateArrivee)
 )
+
+--A faire 
+-------------
+
+--1) Dans la table invite, la valeur de NoInv ne doit pas être divisible par 10 (ajoutez une contrainte).
+--Exemple, 11, 12, 13, ...  
+
+--2) Le numéro de personne dans la table PlanifSoin est soit un no de client, soit un no d’invité.
+
+--3) Les numéros des invités d’un client sont définis comme suit :  
+--Le client 10 a comme invités 11, 12, ..., le client 20 a comme invités 21, 22, ... 
+--Par conséquent, si dans planifSoin, noPers vaut 10 on sait qu’il s’agit d’un client 
+--et si noPersvaut 14, on sait qu’il s’agit d’un invité et que c’est l’invité du client 10.
+
+--4) Dans la table typeSoin, il doit y avoir deux (2) types de soins : Beauté et Santé. Ajoutez ces 2 types de soins
+--
+
+
+--Ajoutez une table typeUtilisateur qui contient un numéro de type d’utilisateurs et son identification. Il existe deux types d’utilisateurs : Admin et PréposéAjoutez une table utilisateur qui contient des utilisateurs. Cette table doit contenir un numéro d’utilisateur (la clef primaire), un nom d’utilisateur (le nom d’utilisateur est unique), un mot de passe et un numéro de type d’utilisateur. Reliez ces deux tables et, dans la table utilisateur, insérez au moins un administrateur et au moins un préposé.  Vous ne devez pas permettre les suppressions en cascade. C’est votre application qui doit gérer les suppressions
